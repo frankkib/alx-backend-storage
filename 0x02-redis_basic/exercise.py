15399a026628a1f480430e3e2cef79bebce7faa2
frankkib <frankkibagendi13@gmail.com>
2023-08-16 11:02:32 +0000

    function that store an instance of the Redis client as a private variable named

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn: Callable = None):
+        """used to convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if value is not None and fn is not None:
+            value = fn(value)
+        return value
+
+    def get_str(self, key):
+        """return cache.get"""
+        return self.get(key, lambda X: X.decode('utf-8'))
+
+    def get_int(self, key):
+        """returns the key"""
+        return self.get(key, int)
