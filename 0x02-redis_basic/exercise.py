18ff7a550596ffbd3a2cdae8ba0adf0da0c83bbe
frankkib <frankkibagendi13@gmail.com>
2023-08-17 10:04:53 +0000

    function that store an instance of the Redis client as a private variable named

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,21 @@ import redis
 from typing import Union, Callable
 
 
+def call_history(method: Callable) -> Callable:
+    """function that increaments values"""
+    def wrapper(*args, **kwargs):
+        """this a function decorator"""
+        function_name = method.__equalname__
+        inputs_key = f"{function_name}:inputs"
+        outputs_key = f"{function_name}:outputs"
+        input_str = str(args)
+        redis_client.rpush(inputs_key, input_str)
+        result = method(*args, **kwargs)
+        redis_client.rpush(outputs_key, result)
+        return result
+    return wrapper
+
+
 class Cache:
     """class initialization"""
     def __init__(self):
